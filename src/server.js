6d27cbdd740fcf409e6236a0c0acf8d64c6093c0
Himanshu Shivgotra <himanshushivgotra@gmail.com>
2025-01-19 22:46:37 +0530

    exploring http methods and routing

--- src/server.js
+++ src/server.js
@@ -2,13 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send('Namaste from the Dashboard');
-})
+// order matter in routing
 
-app.get("/test", (req, res) => {
+app.use("/test", (req, res) => {
     res.send('Hello from the Server');
 })
+
+app.get('/user', (req, res) => {
+    res.send({ firstName: "Himanshu", lastName: "Shivgotra" });
+})
+
+app.post('/user', (req, res) => {
+    res.send("Data saved to the database successfully");
+})
+
+app.delete('/user', (req, res) => {
+    res.send("Data deleted from the database successfully");
+})
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
