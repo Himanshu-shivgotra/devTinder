67a512ea7ba6c3ae0d244b3c22bdc4a3a9b6e309
Himanshu Shivgotra <himanshushivgotra@gmail.com>
2025-01-20 11:24:52 +0530

    dynamic data

--- src/server.js
+++ src/server.js
@@ -3,26 +3,20 @@ const { connectDB } = require('./config/database');
 const app = express();
 const User = require('./models/user')
 
+app.use(express.json())
 
 app.post("/signup", async (req, res) => {
-
-    const user = new User({
-        firstName: "Dure",
-        lastName: "Shivgotra",
-        emailId: "Dure@gmail.com",
-        password: "dure@123",
-        age: "25",
-        gender: "Female",
-    })
-
+    const user = new User(req.body)
     try {
         await user.save()
         res.send("User added Successfully")
-    } catch (err) {
+    }
+    catch (err) {
         res.status(400).send('Error in saving the user', err.message)
     }
 
 })
+
 
 connectDB().then(() => {
     console.log('Database connected.. !!')
@@ -33,5 +27,4 @@ connectDB().then(() => {
     .catch((err) => {
         console.error('Error in connecting database !!')
     })
-
 
