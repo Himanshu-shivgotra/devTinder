350d2b4ea86d80e3ab586845965b45ee9361b5ec
Himanshu Shivgotra <himanshushivgotra@gmail.com>
2025-01-20 12:08:30 +0530

    get API's

--- src/server.js
+++ src/server.js
@@ -14,7 +14,46 @@ app.post("/signup", async (req, res) => {
     catch (err) {
         res.status(400).send('Error in saving the user', err.message)
     }
+})
 
+app.get('/user', async (req, res) => {
+    const userEmail = req.body.emailId;
+    try {
+        const users = await User.find({ emailId: userEmail })
+        if (users.length === 0) {
+            res.status(404).send('User not found')
+        }
+        else {
+            res.send(users)
+        }
+    } catch (err) {
+        res.status(400).send('Something went wrong')
+    }
+})
+
+// app.get('/userById', async (req, res) => {
+//     const userId = req.body.id;
+//     try {
+//         const users = await User.findById({ userId })
+//         res.send(users)
+//     }
+//     catch (err) {
+//         res.status(400).send('Something went wrong');
+//     }
+// })
+app.get('/allUsers', async (req, res) => {
+    try {
+        const users = await User.find({})
+        if (!users) {
+            res.status(404).send('User not found')
+        }
+        else {
+            res.send(users)
+        }
+    }
+    catch (err) {
+        res.status(400).send('Something went wrong')
+    }
 })
 
 
