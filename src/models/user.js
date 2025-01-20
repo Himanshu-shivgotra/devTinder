af3570071b1b0dbbf6dd34ee7e2b6f3af0188f2a
Himanshu Shivgotra <himanshushivgotra@gmail.com>
2025-01-20 15:43:24 +0530

    exploring validator library

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -16,12 +17,22 @@ const userSchema = new mongoose.Schema({
         unique: true,
         lowercase: true,
         trim: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Invalid email :' + value);
+            }
+        }
     },
     password: {
         minLength: 5,
         type: String,
         required: true,
         trim: true,
+        validate(value) {
+            if (!validator.isStrongPassword(value)) {
+                throw new Error('Password is not strong  :' + value);
+            }
+        }
     },
     age: {
         type: Number,
@@ -40,7 +51,12 @@ const userSchema = new mongoose.Schema({
     photoUrl: {
         type: String,
         trim: true,
-        default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png "
+        default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png ",
+        validate(value) {
+            if (!validator.isURL(value)) {
+                throw new Error("Photo url is not valid " + value)
+            }
+        }
     },
     about: {
         type: String,
